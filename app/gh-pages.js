b831feb52dec626deae82024792c2c282fc670a5
ppege <nanguthenangu@gmail.com>
2026-03-12 20:31:41 +0100

    update links to new domains

--- app/gh-pages.js
+++ app/gh-pages.js
@@ -4,14 +4,14 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/NanguRepo/spin', // Update to point to your repository
+  repo: 'https://github.com/ppege/dvd', // Update to point to your repository
   user: {
-   name: 'NanguRepo', // update to use your name
+   name: 'ppege', // update to use your name
    email: 'nanguthenangu@gmail.com' // Update to use your email
   },
   dotfiles: true
-  },
-  () => {
-   console.log('Deploy Complete!');
-  }
+ },
+ () => {
+  console.log('Deploy Complete!');
+ }
 );
